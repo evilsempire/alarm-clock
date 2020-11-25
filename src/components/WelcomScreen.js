81b828900a95b4cc4b865dcf8c6174a24b2533a6
ashuomble5 <ashuomble5@gmail.com>
2020-11-25 23:59:51 +0530

    Initial commit

---
+++ src/components/WelcomScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class WelcomScreen extends React.Component {
+    render() {
+        return <div>
+            WelcomScreen
+        </div>
+    }
+}
+
+export default WelcomScreen;
